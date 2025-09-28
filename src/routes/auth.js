b9382d89f984bc6188598ddfe6a2e9a3e841a424
Sayed Shawaza <ssawaza30@gmail.com>
2025-09-28 23:09:02 +0300

    Update auth.js

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,9 +1,10 @@
 import express from 'express';
-import { authenticate, logout } from '../controllers/authController.js';
+import { authenticate, logout, validateSession } from '../controllers/authController.js';
 
 const router = express.Router();
 
 router.post('/login', authenticate);
 router.post('/logout', logout);
+router.get('/validate', validateSession);
 
 export default router;

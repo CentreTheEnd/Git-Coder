f968ffb0af2f89485a36c72f4627c737eaf59a93
Sayed Shawaza <ssawaza30@gmail.com>
2025-09-28 23:10:36 +0300

    Update files.js

--- src/routes/files.js
+++ src/routes/files.js
@@ -3,8 +3,10 @@ import {
   getRepositoryContents, 
   getFile, 
   updateFile, 
+  createFile, 
   deleteFile, 
-  runCode 
+  runCode,
+  searchInRepo
 } from '../controllers/fileController.js';
 
 const router = express.Router();
@@ -12,7 +14,9 @@ const router = express.Router();
 router.get('/contents', getRepositoryContents);
 router.get('/file', getFile);
 router.put('/file', updateFile);
+router.post('/file', createFile);
 router.delete('/file', deleteFile);
 router.post('/run', runCode);
+router.get('/search', searchInRepo);
 
 export default router;

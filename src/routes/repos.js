5b5ef224c5ef2e372239a6eaa5c0c176ff150374
Sayed Shawaza <ssawaza30@gmail.com>
2025-09-28 23:09:40 +0300

    Update repos.js

--- src/routes/repos.js
+++ src/routes/repos.js
@@ -1,9 +1,10 @@
 import express from 'express';
-import { getRepositories, createRepository } from '../controllers/repoController.js';
+import { getRepositories, createRepository, getRepository } from '../controllers/repoController.js';
 
 const router = express.Router();
 
 router.get('/', getRepositories);
 router.post('/', createRepository);
+router.get('/info', getRepository);
 
 export default router;
